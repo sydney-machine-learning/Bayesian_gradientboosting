b1c116a29bc1f9f55f9d860b3da8917bfa9fc934
George Bai <george.bai@ad.unsw.edu.au>
2021-09-15 17:30:31 +1000

    added code for classification

--- src/run_sequential.py
+++ src/run_sequential.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import yaml
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from torch.optim import SGD, Adam
 
@@ -12,6 +11,7 @@ from data.data import LibTXTData
 from data.sparseloader import DataLoader
 from models.ensemble_net import EnsembleNet
 from models.mlp import MLP_1HL, MLP_2HL
+from utils import auc_score, init_gbnn, root_mse
 
 with open("config.yaml", "r") as yamlfile:
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
@@ -25,15 +25,19 @@ class Config:
 config = Config(**data["params"])
 
 
-def get_data(train_path, test_path):
+def get_data(train_path, test_path, binary=False):
 
-    train = LibTXTData(train_path, config)
-    test = LibTXTData(test_path, config)
+    if binary:
+        train = LibTXTData(train_path, config)
+        test = LibTXTData(test_path, config)
+    else:
+        train = LibTXTData(train_path, config)
+        test = LibTXTData(test_path, config)
 
-    scaler = StandardScaler()
-    scaler.fit(train.feat)
-    train.feat = scaler.transform(train.feat)
-    test.feat = scaler.transform(test.feat)
+        scaler = StandardScaler()
+        scaler.fit(train.feat)
+        train.feat = scaler.transform(train.feat)
+        test.feat = scaler.transform(test.feat)
 
     return train, test
 
@@ -44,127 +48,177 @@ def get_optim(params, lr):
     return optimizer
 
 
-def root_mse(net_ensemble, loader):
-    loss = 0
-    total = 0
+class Experiment:
+    def __init__(self, config):
+        self.config = config
+        self.g_func = None
+        self.lambda_func = None
+        self.loss_func = None
+        self.acc_func = None
+        self.c0 = None
 
-    for x, y in loader:
+    def load_data(self, binary=False):
+        if config.data == "sunspot":
+            train_path = "datasets/Sunspot/train.txt"
+            test_path = "datasets/Sunspot/test.txt"
 
-        if config.cuda:
-            x = x.cuda()
+            config.feat_d = 4
+            config.hidden_d = 10
+            config.out_d = 1
+        if config.data == "rossler":
 
-        with torch.no_grad():
-            out = net_ensemble.forward(x)
+            train_path = "datasets/Rossler/train.txt"
+            test_path = "datasets/Rossler/test.txt"
 
-        y = y.cpu().numpy().reshape(len(y), 1)
+            config.feat_d = 4
+            config.hidden_d = 10
+            config.out_d = 1
+        if config.data == "ionosphere":
 
-        out = out.cpu().numpy().reshape(len(y), 1)
-        loss += mean_squared_error(y, out) * len(y)
-        total += len(y)
-    return np.sqrt(loss / total)
+            train_path = "datasets/Ionosphere/train.txt"
+            test_path = "datasets/Ionosphere/test.txt"
 
+            config.feat_d = 34
+            config.hidden_d = 50
+            config.out_d = 1
+        if config.data == "cancer":
 
-def run_experiment(train, test, num_nets):
-    model_type = MLP_1HL
+            train_path = "datasets/Cancer/train.txt"
+            test_path = "datasets/Cancer/test.txt"
 
-    c0 = np.mean(train.label)
-    net_ensemble = EnsembleNet(c0, config.lr)
+            config.feat_d = 9
+            config.hidden_d = 12
+            config.out_d = 1
 
-    train_loader = DataLoader(train, 1, shuffle=True, drop_last=False, num_workers=2)
-    test_loader = DataLoader(test, 1, shuffle=False, drop_last=False, num_workers=2)
+        self.train, self.test = get_data(train_path, test_path, binary=binary)
 
-    loss_f = nn.MSELoss()
+    def init_experiment(self):
+        if self.config.data in ["sunspot", "rossler"]:  # Regression problems
+            self.load_data()
+            self.init_regression()
+            self.config.classification = False
+        elif self.config.data in ["ionosphere", "cancer"]:  # Classification problems
+            self.load_data(binary=True)
+            self.init_classification()
+            self.config.classification = True
 
-    final_tr_rmse = 0
-    final_te_rmse = 0
-    total_time = 0
-    for stage in range(num_nets):
+    def init_regression(self):
+        self.g_func = lambda y, yhat: 2 * (yhat - y)
+        self.lambda_func = lambda y, fx, Fx: torch.sum(fx * (y - Fx)) / torch.sum(fx * fx)
+        self.loss_func = nn.MSELoss()
+        self.acc_func = root_mse
 
-        model = model_type.get_model(config)
-        if config.cuda:
-            model.cuda()
+        self.c0 = np.mean(self.train.label)
 
-        optimizer = get_optim(model.parameters(), config.lr)
-        net_ensemble.to_train()  # Set the models in ensemble net to train mode
+    def init_classification(self):
+        self.g_func = lambda y, yhat: -1 * (2 * y) / (1 + torch.exp(2 * y * yhat))
+        # self.g_func = lambda y, yhat: y - yhat
 
-        for i, (x, y) in enumerate(train_loader):
+        # self.lambda_func = lambda y, fx, Fx: torch.sum(1 / fx * (y - Fx) / (Fx * (1 - Fx)))
+        self.lambda_func = lambda y, fx, Fx: 1
+        self.loss_func = nn.MSELoss()
+        self.acc_func = auc_score
+
+        self.c0 = init_gbnn(self.train)
+
+    def run_experiment(self):
+
+        for level in range(1, config.num_nets + 1):
+
+            t0 = time.time()
+
+            tr_score_l = []
+            te_score_l = []
+            for _ in range(config.exps):
+                tr_rmse, te_rmse = self.run(self.train, self.test, level)
+                tr_score_l.append(tr_rmse)
+                te_score_l.append(te_rmse)
+
+            total_time = time.time() - t0
+
+            print(f"Boosting level: {level}")
+            if self.config.classification:
+                print(
+                    f"Training statistics: mean - {np.mean(tr_score_l)}, std = {np.std(tr_score_l)}, best = {np.amax(tr_score_l)}"
+                )
+                print(
+                    f"Test statistics: mean - {np.mean(te_score_l)}, std = {np.std(te_score_l)}, best = {np.amax(te_score_l)}"
+                )
+            else:
+                print(
+                    f"Training statistics: mean - {np.mean(tr_score_l)}, std = {np.std(tr_score_l)}, best = {np.amin(tr_score_l)}"
+                )
+                print(
+                    f"Test statistics: mean - {np.mean(te_score_l)}, std = {np.std(te_score_l)}, best = {np.amin(te_score_l)}"
+                )
+            print(f"Total elapsed time: {total_time}")
+
+    def run(self, train, test, num_nets):
+        model_type = MLP_1HL
+
+        net_ensemble = EnsembleNet(self.c0, config.lr)
+
+        train_loader = DataLoader(train, 1, shuffle=True, drop_last=False, num_workers=2)
+        test_loader = DataLoader(test, 1, shuffle=False, drop_last=False, num_workers=2)
+
+        final_tr_score = 0
+        final_te_score = 0
+        for stage in range(num_nets):
+
+            model = model_type.get_model(config)
+            if config.cuda:
+                model.cuda()
+
+            optimizer = get_optim(model.parameters(), config.lr)
+            net_ensemble.to_train()  # Set the models in ensemble net to train mode
+
+            for i, (x, y) in enumerate(train_loader):
+
+                if config.cuda:
+                    x = x.cuda()
+                    y = torch.as_tensor(y, dtype=torch.float32).cuda().view(-1, 1)
+                out = net_ensemble.forward(x)
+                out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
+
+                grad_direction = -1 * self.g_func(y, out)
+
+                out = model(x)
+                out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
+                loss = self.loss_func(out, grad_direction)
+
+                model.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            # Calculate gamma
+            x = torch.tensor(train.feat).cuda()
+            y = torch.tensor(train.label).cuda()
+
+            gamma = self.lambda_func(y, model(x), net_ensemble.forward(x))
+
+            net_ensemble.add(model, gamma)
 
             if config.cuda:
-                x = x.cuda()
-                y = torch.as_tensor(y, dtype=torch.float32).cuda().view(-1, 1)
-            out = net_ensemble.forward(x)
-            out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
+                net_ensemble.to_cuda()
+            net_ensemble.to_eval()  # Set the models in ensemble net to eval mode
 
-            grad_direction = -2 * (out - y)
+            # Train
+            final_tr_score = self.acc_func(net_ensemble, train_loader)
 
-            out = model(x)
-            out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
-            loss = loss_f(out, grad_direction)  # T
+            final_te_score = self.acc_func(net_ensemble, test_loader)
 
-            model.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # print(
+            #     f"Stage: {stage}  RMSE@Tr: {tr_rmse:.5f}, RMSE@Val: {val_rmse:.5f}, RMSE@Te: {te_rmse:.5f}"
+            # )
 
-        # Calculate gamma
-        x = torch.tensor(train.feat).cuda()
-        y = torch.tensor(train.label).cuda()
-        fx = model(x)
+        # print(f"Stage: {num_nets}  RMSE@Tr: {tr_rmse:.5f}, final RMSE@Te: {te_rmse:.5f}")
 
-        gamma = torch.sum(fx * (y - net_ensemble.forward(x))) / torch.sum(fx * fx)
-
-        net_ensemble.add(model, gamma)
-
-        if config.cuda:
-            net_ensemble.to_cuda()
-        net_ensemble.to_eval()  # Set the models in ensemble net to eval mode
-
-        # Train
-        tr_rmse = root_mse(net_ensemble, train_loader)
-        final_tr_rmse = tr_rmse
-
-        te_rmse = root_mse(net_ensemble, test_loader)
-        final_te_rmse = te_rmse
-
-        # print(
-        #     f"Stage: {stage}  RMSE@Tr: {tr_rmse:.5f}, RMSE@Val: {val_rmse:.5f}, RMSE@Te: {te_rmse:.5f}"
-        # )
-
-    # print(f"Stage: {num_nets}  RMSE@Tr: {tr_rmse:.5f}, final RMSE@Te: {te_rmse:.5f}")
-
-    return final_tr_rmse, final_te_rmse, total_time
+        return final_tr_score, final_te_score
 
 
 if __name__ == "__main__":
 
-    if config.data == "sunspot":
-        train_path = "datasets/Sunspot/train.txt"
-        test_path = "datasets/Sunspot/test.txt"
-
-        config.feat_d = 4
-        config.hidden_d = 10
-        config.out_d = 1
-
-    train, test = get_data(train_path, test_path)
-
-    for level in range(1, config.num_nets + 1):
-
-        t0 = time.time()
-
-        tr_rmse_l = []
-        te_rmse_l = []
-        for _ in range(config.exps):
-            tr_rmse, te_rmse, runtime = run_experiment(train, test, level)
-            tr_rmse_l.append(tr_rmse)
-            te_rmse_l.append(te_rmse)
-
-        total_time = time.time() - t0
-
-        print(f"Boosting level: {level}")
-        print(
-            f"Training statistics: mean - {np.mean(tr_rmse_l)}, std = {np.std(tr_rmse_l)}, best = {np.amin(tr_rmse_l)}"
-        )
-        print(
-            f"Test statistics: mean - {np.mean(te_rmse_l)}, std = {np.std(te_rmse_l)}, best = {np.amin(te_rmse_l)}"
-        )
-        print(f"Total elapsed time: {total_time}")
+    exp = Experiment(config)
+    exp.init_experiment()
+    exp.run_experiment()
 
---
+++ src/utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+import torch
+from sklearn.metrics import mean_squared_error
+
+
+def root_mse(net_ensemble, loader, cuda=True):
+    loss = 0
+    total = 0
+
+    for x, y in loader:
+
+        if cuda:
+            x = x.cuda()
+
+        with torch.no_grad():
+            out = net_ensemble.forward(x)
+
+        y = y.cpu().numpy().reshape(len(y), 1)
+
+        out = out.cpu().numpy().reshape(len(y), 1)
+        loss += mean_squared_error(y, out) * len(y)
+        total += len(y)
+    return np.sqrt(loss / total)
+
+
+def auc_score(net_ensemble, test_loader, cuda=True):
+    actual = []
+    posterior = []
+    for x, y in test_loader:
+        if cuda:
+            x = x.cuda()
+        with torch.no_grad():
+            out = net_ensemble.forward(x)
+        prob = 1.0 - 1.0 / torch.exp(
+            out
+        )  # Why not using the scores themselve than converting to prob
+        prob = prob.cpu().numpy().tolist()
+
+        if type(prob) is float:
+            posterior.append(prob)
+            actual.append(y)
+        else:
+            posterior.extend(prob)
+            actual.extend(y.numpy().tolist())
+    score = auc(actual, posterior)
+    return score
+
+
+def tied_rank(x):
+    """
+    Computes the tied rank of elements in x.
+
+    This function computes the tied rank of elements in x.
+
+    Parameters
+    ----------
+    x : list of numbers, numpy array
+
+    Returns
+    -------
+    score : list of numbers
+            The tied rank f each element in x
+
+    """
+    sorted_x = sorted(zip(x, range(len(x))))
+    r = [0 for k in x]
+    cur_val = sorted_x[0][0]
+    last_rank = 0
+    for i in range(len(sorted_x)):
+        if cur_val != sorted_x[i][0]:
+            cur_val = sorted_x[i][0]
+            for j in range(last_rank, i):
+                r[sorted_x[j][1]] = float(last_rank + 1 + i) / 2.0
+            last_rank = i
+        if i == len(sorted_x) - 1:
+            for j in range(last_rank, i + 1):
+                r[sorted_x[j][1]] = float(last_rank + i + 2) / 2.0
+    return r
+
+
+def auc(actual, posterior):
+    """
+    Computes the area under the receiver-operater characteristic (AUC)
+
+    This function computes the AUC error metric for binary classification.
+
+    Parameters
+    ----------
+    actual : list of binary numbers, numpy array
+             The ground truth value
+    posterior : same type as actual
+                Defines a ranking on the binary numbers, from most likely to
+                be positive to least likely to be positive.
+
+    Returns
+    -------
+    score : double
+            The mean squared error between actual and posterior
+
+    """
+    r = tied_rank(posterior)
+    num_positive = len([0 for x in actual if x == 1])
+    num_negative = len(actual) - num_positive
+    sum_positive = sum([r[i] for i in range(len(r)) if actual[i] == 1])
+    auc = (sum_positive - num_positive * (num_positive + 1) / 2.0) / (num_negative * num_positive)
+    return auc
+
+
+def init_gbnn(train):
+    positive = negative = 0
+    for i in range(len(train)):
+        if train[i][1] > 0:
+            positive += 1
+        else:
+            negative += 1
+    blind_acc = max(positive, negative) / (positive + negative)
+    return float(np.log(positive / negative))
